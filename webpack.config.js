1619f56b2516b5795fc7674dff333e5df7695455
Yongrok Oh <ohyr96@ajou.ac.kr>
2020-07-14 17:41:25 +0900

    Setup crossbrowsing, autoprefixer
    1. postcss
    2. postcss-preset-env

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,86 @@
-const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const PostcssPresetEnv = require('postcss-preset-env');
 
 module.exports = {
   entry: {
     main: './src/index.js',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+      hash: true,
+    }),
+    new MiniCssExtractPlugin({
+    }),
+  ],
   module: {
     rules: [
       {
         test: /\.js$/,
-        include: path.join(__dirname),
-        exclude: /(node_modules)|(dist)/,
+        exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
         },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        oneOf: [
+          {
+            issuer: /\.html$/,
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  onlyLocals: true,
+                  modules: true,
+                },
+              },
+              {
+                loader: 'postcss-loader',
+                options: {
+                  plugins: [
+                    PostcssPresetEnv({
+                      autoprefixer: { grid: true },
+                      browsers: ['> 1%', 'last 2 versions'],
+                    }),
+                  ],
+                },
+              },
+              'sass-loader',
+            ],
+          },
+          {
+            use: [
+              MiniCssExtractPlugin.loader,
+              // {
+              //   loader: MiniCssExtractPlugin.loader,
+              // },
+              {
+                loader: 'css-loader',
+                options: {
+                  modules: true,
+                },
+              },
+              {
+                loader: 'postcss-loader',
+                options: {
+                  plugins: [
+                    PostcssPresetEnv({
+                      autoprefixer: { grid: true },
+                      browsers: ['> 1%', 'last 2 versions'],
+                    }),
+                  ],
+                },
+              },
+              'sass-loader',
+              // {
+              //   loader: 'sass-loader',
+              // },
+            ],
+          },
+        ],
       },
     ],
   },
