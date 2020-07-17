6f32dd52a7fe39ce12016f43b55d35c76dd09749
Yongrok Oh <ohyr96@ajou.ac.kr>
2020-07-17 12:10:36 +0900

    Create adding bucketlist
    1. 등록하려는 Text와 등록 날짜를 List에 등록하는 기능 구현
    2. 버킷리스트 Title, Text 입력창, 버튼 위치 수정

--- src/index.js
+++ src/index.js
@@ -1,1 +1,50 @@
-console.log('Hello, world!');
+import './style.scss';
+
+const { document } = global;
+
+/*
+// Todo: In progress...
+function storeBucket() {
+  if (typeof (Storage) !== 'undefined') {
+    // Code for localStorage/sessionStorage.
+  } else {
+    // Sorry! No Web Storage support..
+  }
+}
+*/
+
+const addBucketItem = (li, ul) => ul.appendChild(li);
+
+function makeBucketItem(text) {
+  const li = document.createElement('li');
+  const createdDate = new Date();
+  const fullText = `${text}\t...\t${createdDate}`;
+  const textNode = document.createTextNode(fullText);
+  li.appendChild(textNode);
+
+  return li;
+}
+
+function btnHandler(e) {
+  e.preventDefault();
+
+  const ul = document.querySelector('.js-bucketlist');
+  const inputText = document.querySelector('.js-inputText').value;
+  const li = makeBucketItem(inputText);
+
+  addBucketItem(li, ul);
+}
+
+function init() {
+  const btn = document.querySelector('.js-btn');
+  btn.addEventListener('click', btnHandler);
+
+  document.querySelector('.js-bucketlist').addEventListener('click', (ev) => {
+    if (ev.target.tagName === 'LI') {
+      ev.target.classList.toggle('checked');
+      console.log(ev.target);
+    }
+  }, false);
+}
+
+init();
